79e16b911e94e3b42052a2c0dbb6bd5edc9b6e68
Vagner <haigorne@gmail.com>
2024-12-02 14:25:00 -0300

    [mod-4] Exemplo basico persistencia

--- libraryapi/src/main/java/io/github/cursodsousa/libraryapi/LibraryapiApplication.java
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/LibraryapiApplication.java
@@ -1,13 +1,30 @@
 package io.github.cursodsousa.libraryapi;
 
+import io.github.cursodsousa.libraryapi.model.Autor;
+import io.github.cursodsousa.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class LibraryapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryapiApplication.class, args);
+		//SpringApplication.run(LibraryapiApplication.class, args);
+		var context = SpringApplication.run(LibraryapiApplication.class, args);
+		AutorRepository repository = context.getBean(AutorRepository.class);
+		exemploSalvarRegistro(repository);
+	}
+
+	public static void exemploSalvarRegistro(AutorRepository autorRepository){
+		Autor autor = new Autor();
+		autor.setNome("José S.");
+		autor.setDataNascimento(LocalDate.of(1950,1,1));
+		autor.setNacionalidade("Brasileira");
+
+		var autorSalvo = autorRepository.save(autor);
+		System.out.println("Autor Salvo:" + autorSalvo);
 	}
 
 }
