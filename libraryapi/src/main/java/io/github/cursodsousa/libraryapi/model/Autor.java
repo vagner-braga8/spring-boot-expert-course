bfea68b771c211946e30df63bc46d2bc3c60dc5d
Vagner <haigorne@gmail.com>
2024-12-07 23:21:14 -0300

    [mod-5] Adicionando Auditoria JPA nas entidades
    Antes de começar:
    Drope as tabelas de livro e autor;
    Rode os scripts do arquivo comandos-sql.txt

--- libraryapi/src/main/java/io/github/cursodsousa/libraryapi/model/Autor.java
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/model/Autor.java
@@ -4,8 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,6 +18,7 @@ import java.util.UUID;
 @Getter
 @Setter
 @ToString(exclude = "livros" )
+@EntityListeners(AuditingEntityListener.class) //
 public class Autor {
 
     @Id
@@ -29,7 +34,19 @@ public class Autor {
     @Column(length = 50, nullable = false)
     private String nacionalidade;
 
-    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL)  //'one' entidade atual, 'many' entidade relacionada
+    @OneToMany(mappedBy = "autor", cascade = CascadeType.ALL)
     private List<Livro> livros;
+
+    @CreatedDate
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @Column(name = "id_usuario")
+    private UUID idUsuario;
+
 
 }
