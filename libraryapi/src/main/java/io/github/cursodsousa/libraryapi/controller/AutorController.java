91ce87a910a7487adc2f571ff78021e9afb97d52
Vagner <haigorne@gmail.com>
2024-12-07 23:04:35 -0300

    [mod-5] Mapeamento de Requisição e padrão DTO e Save Autor

---
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/controller/AutorController.java
@@ -0,0 +1,39 @@
+package io.github.cursodsousa.libraryapi.controller;
+
+import io.github.cursodsousa.libraryapi.controller.dto.AutorDTO;
+import io.github.cursodsousa.libraryapi.service.AutorService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/autores")
+public class AutorController {
+
+    private final AutorService autorService;
+
+    public AutorController(AutorService autorService) {
+        this.autorService = autorService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> salvar(@RequestBody AutorDTO autor){
+        var autorEntidade = autor.mapearParaAutor();
+        autorService.salvar(autorEntidade);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(autorEntidade.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+}
---
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/service/AutorService.java
@@ -0,0 +1,19 @@
+package io.github.cursodsousa.libraryapi.service;
+
+import io.github.cursodsousa.libraryapi.model.Autor;
+import io.github.cursodsousa.libraryapi.repository.AutorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutorService {
+
+    private final AutorRepository autorRepository;
+
+    public AutorService(AutorRepository autorRepository){
+        this.autorRepository = autorRepository;
+    }
+
+    public Autor salvar(Autor autor) {
+        return autorRepository.save(autor);
+    }
+}
