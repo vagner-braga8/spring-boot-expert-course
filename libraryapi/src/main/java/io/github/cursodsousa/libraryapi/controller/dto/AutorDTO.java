88904a30f0c51ebdc17371983fd36919d6bc5830
Vagner <haigorne@gmail.com>
2024-12-11 10:46:53 -0300

    [mod-6] Mais validações do Spring

--- libraryapi/src/main/java/io/github/cursodsousa/libraryapi/controller/dto/AutorDTO.java
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/controller/dto/AutorDTO.java
@@ -3,6 +3,8 @@ package io.github.cursodsousa.libraryapi.controller.dto;
 import io.github.cursodsousa.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -11,12 +13,15 @@ public record AutorDTO(
         UUID id,
 
         @NotBlank(message = "campo obrigatorio")
+        @Size(min = 2,max = 100, message = "campo fora do tamanho padrao")
         String nome,
 
         @NotNull(message = "campo obrigatorio")
+        @Past(message = "nao pode ser uma data futura")
         LocalDate dataNascimento,
 
         @NotBlank(message = "campo obrigatorio")
+        @Size(min = 2,max = 50, message = "campo fora do tamanho padrao")
         String nacionalidade) {
 
     public Autor mapearParaAutor(){
