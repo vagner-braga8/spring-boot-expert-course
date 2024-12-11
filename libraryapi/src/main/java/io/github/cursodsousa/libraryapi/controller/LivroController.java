630b08cdacf651f55418e693b90dd124ac90be67
Vagner <haigorne@gmail.com>
2024-12-11 15:45:23 -0300

    [mod-7] Endpoint para cadastro de livro com Bean Validation

--- libraryapi/src/main/java/io/github/cursodsousa/libraryapi/controller/LivroController.java
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/controller/LivroController.java
@@ -1,7 +1,15 @@
 package io.github.cursodsousa.libraryapi.controller;
 
+import io.github.cursodsousa.libraryapi.controller.dto.CadastroLivroDTO;
+import io.github.cursodsousa.libraryapi.controller.dto.ErroResposta;
+import io.github.cursodsousa.libraryapi.exceptions.RegistroDuplicadoException;
+import io.github.cursodsousa.libraryapi.model.Livro;
 import io.github.cursodsousa.libraryapi.service.LivroService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class LivroController {
 
     private LivroService livroService;
+
+    @PostMapping
+    public ResponseEntity<Object> salvar(@RequestBody @Valid CadastroLivroDTO cadastroLivroDTO) {
+        try {
+            // Mapear dto para entidade
+            //enviar a enteidade para o service validar e salvar
+            //criar url para acesso dos dados do livro
+            //retornar codigo created com header location
+            return ResponseEntity.ok(cadastroLivroDTO);
+        }catch (RegistroDuplicadoException e) {
+            var erroDTO = ErroResposta.conflito(e.getMessage());
+            return ResponseEntity.status(erroDTO.status()).body(erroDTO);
+        }
+    }
+
 }
