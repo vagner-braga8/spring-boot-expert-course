84083949f4966ee260c0b260489f867a71d03652
Vagner <haigorne@gmail.com>
2024-12-28 13:43:57 -0300

    [mod-9] Eliminando o prefixo ROLE do security

--- libraryapi/src/main/java/io/github/cursodsousa/libraryapi/config/SecurityConfiguration.java
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/config/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -52,4 +53,8 @@ public class SecurityConfiguration {
         return new CustomUserDetailsService(usuarioService);
     }
 
+    @Bean
+    public GrantedAuthorityDefaults grantedAuthorityDefaults(){
+        return new GrantedAuthorityDefaults(""); //Ignorando o prefixo padrão *'ROLE_'
+    }
 }
--- libraryapi/src/main/java/io/github/cursodsousa/libraryapi/security/CustomAuthentication.java
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/security/CustomAuthentication.java
@@ -21,7 +21,7 @@ public class CustomAuthentication implements Authentication {
         return this.usuario
                 .getRoles()
                 .stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
     }
 
