77304dadefbb875d000b18e6f3ae8792bb0c1a81
Vagner <haigorne@gmail.com>
2024-12-28 22:15:11 -0300

    [mod-9] Lógica para novos usuários que só tem login Google

--- libraryapi/src/main/java/io/github/cursodsousa/libraryapi/security/LoginSocialSuccessHandler.java
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/security/LoginSocialSuccessHandler.java
@@ -8,16 +8,20 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    private static final String SENHA_PADRAO = "123";
 
     private final UsuarioService usuarioService;
 
@@ -33,10 +37,30 @@ public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSu
 
         Usuario usuario = usuarioService.obterPorEmail(email);
 
+        if(usuario == null){
+            usuario = criarUsuario(email);
+        }
+
+
         authentication = new CustomAuthentication(usuario);
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         super.onAuthenticationSuccess(request, response, authentication);
+    }
+
+    private Usuario criarUsuario(String email) {
+        Usuario usuario;
+        usuario = new Usuario();
+        usuario.setEmail(email);
+        usuario.setLogin(extrairPrefixoEmail(email));
+        usuario.setSenha(SENHA_PADRAO);
+        usuario.setRoles(List.of("USUARIO"));
+        usuarioService.salvar(usuario);
+        return usuario;
+    }
+
+    private String extrairPrefixoEmail(String email) {
+        return email.substring(0, email.indexOf("@"));
     }
 }
