203abf43fd0f94c224168b4dd4420ea94ddd0ff8
Vagner <haigorne@gmail.com>
2024-12-30 14:13:41 -0300

    [mod-10]Criando o RegisteredClientsRepository customizado

--- libraryapi/src/main/java/io/github/cursodsousa/libraryapi/service/ClientService.java
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/service/ClientService.java
@@ -6,9 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.UUID;
-
 @Service
 @RequiredArgsConstructor
 public class ClientService {
