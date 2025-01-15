d8806d34b43e0a5b5ba9070cf6e1f1c3b17ba619
Vagner <haigorne@gmail.com>
2025-01-14 21:07:45 -0300

    [mod-10] Obtendo um token JWT utilizando Client Credentials grant type

--- libraryapi/src/main/java/io/github/cursodsousa/libraryapi/config/AuthorizationServerConfiguration.java
+++ libraryapi/src/main/java/io/github/cursodsousa/libraryapi/config/AuthorizationServerConfiguration.java
@@ -71,7 +71,7 @@ public class AuthorizationServerConfiguration {
     @Bean
     public JWKSource<SecurityContext> jwkSource() throws Exception {
         RSAKey rsaKey = gerarChaveRSA();
-        JWKSet jwkSet = new JWKSet();
+        JWKSet jwkSet = new JWKSet(rsaKey);
         return new ImmutableJWKSet<>(jwkSet);
     }
 
